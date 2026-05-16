accf3ca97197016aeaca8697749d508c1564e71e
Zales <111923813+96GT@users.noreply.github.com>
2026-05-16 15:19:13 -0700

    Update queries.sql

--- sql-projects/superstore-sales-analysis/queries.sql
+++ sql-projects/superstore-sales-analysis/queries.sql
@@ -1,1 +1,10 @@
 
+-- My first SQL practice query
+-- Goal: Find total sales by product category
+
+SELECT
+  category,
+  SUM(sales) AS total_sales
+FROM superstore
+GROUP BY category
+ORDER BY total_sales DESC;
